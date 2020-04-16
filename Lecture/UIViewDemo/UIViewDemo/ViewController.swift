e38e3de044aac9f8d14c3a7d8d90d607ea9bf59a
kaden-kykim <kaden.kykim@gmail.com>
2020-04-16 14:26:59 -0700

    Change adding views by programmatically (Apr. 16)

--- Lecture/UIViewDemo/UIViewDemo/ViewController.swift
+++ Lecture/UIViewDemo/UIViewDemo/ViewController.swift
@@ -10,9 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let redView: UIView = {
+       let rView = UIView()
+        rView.backgroundColor = .red
+        rView.translatesAutoresizingMaskIntoConstraints = false
+        return rView
+    }()
+    
+    let profileIV: UIImageView = {
+        let iv = UIImageView()
+        iv.image = UIImage(named: "van")
+        iv.contentMode = .scaleAspectFit
+        iv.clipsToBounds = true
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        return iv
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+//        print("frame (\(redView.frame.origin.x), \(redView.frame.origin.y))")
+//        print("bounds (\(redView.bounds.origin.x), \(redView.bounds.origin.y))")
+//        print(redView.bounds.size.width)
+        
+        let width = view.bounds.size.width * 0.7
+        redView.widthAnchor.constraint(equalToConstant: width).isActive = true
+        redView.heightAnchor.constraint(equalToConstant: width).isActive = true
+        profileIV.widthAnchor.constraint(equalToConstant: width).isActive = true
+        profileIV.heightAnchor.constraint(equalToConstant: width).isActive = true
+        
+        let vStackView = UIStackView(arrangedSubviews: [redView, profileIV])
+        vStackView.translatesAutoresizingMaskIntoConstraints = false
+        vStackView.axis = .vertical
+        vStackView.distribution = .equalSpacing
+        vStackView.alignment = .center
+        vStackView.spacing = 20
+        
+        view.addSubview(vStackView)
+        vStackView.topAnchor.constraint(equalTo: view.topAnchor, constant: 44).isActive = true
+        vStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
     }
 
 
