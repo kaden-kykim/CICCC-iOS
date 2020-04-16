4a940bfcd60e0e3274e325dcb121f3c52e3017e3
kaden-kykim <kaden.kykim@gmail.com>
2020-04-16 14:00:27 -0700

    Add UIViewDemo project in class (Apr. 16)

---
+++ Lecture/UIViewDemo/UIViewDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIViewDemo
+//
+//  Created by Kaden Kim on 2020-04-16.
+//  Copyright © 2020 CICCC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
