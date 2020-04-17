d890e4daa980ab444bf092de4524fdd3bf60d3f7
kaden-kykim <kaden.kykim@gmail.com>
2020-04-17 10:32:38 -0700

    [Guided Project 1] Add a simple button to change background color

--- Lab/Light/Light/ViewController.swift
+++ Lab/Light/Light/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var lightOn = true
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func buttonPressed(_ sender: Any) {
+        lightOn = !lightOn
+        updateUI()
+    }
+    
+    func updateUI() {
+        if lightOn {
+            view.backgroundColor = .white
+        } else {
+            view.backgroundColor = .black
+        }
+    }
 }
 
