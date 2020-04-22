94eeccfef859e6934e4612811d21ac342271bb90
kaden-kykim <kaden.kykim@gmail.com>
2020-04-22 13:00:53 -0700

    [Guided Project 2] Add Part 1 - Build the Interface

--- Lab/ApplePie/ApplePie/ViewController.swift
+++ Lab/ApplePie/ApplePie/ViewController.swift
@@ -10,11 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var treeImageView: UIImageView!
+    @IBOutlet var correctWordLabel: UILabel!
+    @IBOutlet var scoreLabel: UILabel!
+    @IBOutlet var letterButton: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        sender.isEnabled = false
+    }
+    
 }
 
