b7ed45ec63961071233e00a2bd6461d6b1e80aa3
kaden-kykim <kaden.kykim@gmail.com>
2020-04-28 13:40:37 -0700

    [Life Cycle (Apr. 28)] Create project - ScrollingForm - for reviewing Life Cycle at first

---
+++ Lecture/ScrollingForm/ScrollingForm/SceneDelegate.swift
@@ -0,0 +1,62 @@
+//
+//  SceneDelegate.swift
+//  ScrollingForm
+//
+//  Created by Kaden Kim on 2020-04-28.
+//  Copyright © 2020 CICCC. All rights reserved.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+    var viewController: ViewController?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+        
+        viewController = window?.rootViewController as? ViewController
+        print(#function)
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+        // Called as the scene is being released by the system.
+        // This occurs shortly after the scene enters the background, or when its session is discarded.
+        // Release any resources associated with this scene that can be re-created the next time the scene connects.
+        // The scene may re-connect later, as its session was not neccessarily discarded (see `application:didDiscardSceneSessions` instead).
+        print(#function)
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+        // Called when the scene has moved from an inactive state to an active state.
+        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+        print(#function)
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+        // Called when the scene will move from an active state to an inactive state.
+        // This may occur due to temporary interruptions (ex. an incoming phone call).
+        print(#function)
+        viewController?.counter += 1
+        print("Counter: \(viewController!.counter)")
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+        // Called as the scene transitions from the background to the foreground.
+        // Use this method to undo the changes made on entering the background.
+        print(#function)
+        viewController?.counter += 1
+        print("Counter: \(viewController!.counter)")
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        // Called as the scene transitions from the foreground to the background.
+        // Use this method to save data, release shared resources, and store enough scene-specific state information
+        // to restore the scene back to its current state.
+        print(#function)
+    }
+
+
+}
+
---
+++ Lecture/ScrollingForm/ScrollingForm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScrollingForm
+//
+//  Created by Kaden Kim on 2020-04-28.
+//  Copyright © 2020 CICCC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var counter = 0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
