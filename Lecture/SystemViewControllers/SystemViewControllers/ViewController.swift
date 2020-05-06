72e802d48401a33e06a304606f5f1fecfa38c3f0
kaden-kykim <kaden.kykim@gmail.com>
2020-05-06 13:45:55 -0700

    [Lesson 4.8 (May 6)] Add UI and Safari system view

--- Lecture/SystemViewControllers/SystemViewControllers/ViewController.swift
+++ Lecture/SystemViewControllers/SystemViewControllers/ViewController.swift
@@ -7,14 +7,36 @@
 //
 
 import UIKit
+import SafariServices
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func shareButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func safariButtonTapped(_ sender: UIButton) {
+        // 1. url
+        if let url = URL(string: "https://www.google.ca") {
+            // 2. create safari view controller
+            let safariVC = SFSafariViewController(url: url)
+            present(safariVC, animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func camaraButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func emailButtonTapped(_ sender: UIButton) {
+        
+    }
+    
 }
-
