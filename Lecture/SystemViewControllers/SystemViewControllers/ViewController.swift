58a94f89afeaa2487f55095d6ce22ade267a9c8c
kaden-kykim <kaden.kykim@gmail.com>
2020-05-06 13:36:23 -0700

    [Lesson 4.8 (May 6)] Add SystemViewConrollers project

---
+++ Lecture/SystemViewControllers/SystemViewControllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SystemViewControllers
+//
+//  Created by Kaden Kim on 2020-05-06.
+//  Copyright © 2020 CICCC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
