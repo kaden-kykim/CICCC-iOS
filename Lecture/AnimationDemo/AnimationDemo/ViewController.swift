9f946faa554c309f2ca9ec65ac276f8cfd5d2931
kaden-kykim <kaden.kykim@gmail.com>
2020-05-12 13:59:45 -0700

    [Lesson 5.3 (May 12)] Create AnimationDemo project

---
+++ Lecture/AnimationDemo/AnimationDemo/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  AnimationDemo
+//
+//  Created by Kaden Kim on 2020-05-12.
+//  Copyright © 2020 CICCC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+}
