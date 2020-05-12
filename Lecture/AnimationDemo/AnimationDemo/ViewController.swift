26ae04f685d71f8dd85fed50a706489dcfa64a3a
kaden-kykim <kaden.kykim@gmail.com>
2020-05-12 14:03:57 -0700

    [Lesson 5.3 (May 12)] Add the first animation: changing background color

--- Lecture/AnimationDemo/AnimationDemo/ViewController.swift
+++ Lecture/AnimationDemo/AnimationDemo/ViewController.swift
@@ -13,6 +13,18 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        
+        example1()
+    }
+
+    // change the background color of the square view
+    func example1() {
+        let square = UIView(frame: .init(x: 0, y: 44, width: 100, height: 100))
+        square.backgroundColor = .purple
+        view.addSubview(square)
+        UIView.animate(withDuration: 3.0) {
+            square.backgroundColor = .orange
+        }
     }
 
 }
