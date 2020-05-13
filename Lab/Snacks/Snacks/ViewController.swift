6437e86ce0696bb820093c3f1436930b040e3c72
kaden-kykim <kaden.kykim@gmail.com>
2020-05-13 15:48:41 -0700

    [Assignment 7] Create Snacks project and Add assets

---
+++ Lab/Snacks/Snacks/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Snacks
+//
+//  Created by Kaden Kim on 2020-05-13.
+//  Copyright © 2020 CICCC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+}
