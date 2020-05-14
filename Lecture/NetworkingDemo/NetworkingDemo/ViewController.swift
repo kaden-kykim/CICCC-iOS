1a77f10e9ecb3e479d6cfccc27f3efd3205e50cf
kaden-kykim <kaden.kykim@gmail.com>
2020-05-14 13:39:11 -0700

    [Lesson 5.4 (May 14)] Create NetworkingDemo project

---
+++ Lecture/NetworkingDemo/NetworkingDemo/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  NetworkingDemo
+//
+//  Created by Kaden Kim on 2020-05-14.
+//  Copyright © 2020 CICCC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
