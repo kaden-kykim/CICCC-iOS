7d0ae791c5fca58f753a9f724a08c44c55190a3f
kaden-kykim <kaden.kykim@gmail.com>
2020-05-14 13:00:31 -0700

    [Assignment 7] Add Navigation Bar's Animation: Expand/Collapse, Rotating Plus Icon, Spring effect

--- Lab/Snacks/Snacks/ViewController.swift
+++ Lab/Snacks/Snacks/ViewController.swift
@@ -10,14 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    private static let NAV_BAR_HEIGHT: CGFloat = 44
-    private static let STAT_BAR_HEIGHT: CGFloat = 20
+    private let STAT_BAR_HEIGHT: CGFloat = 20
+    private let NAV_BAR_HEIGHT: CGFloat = 44
+    private let NAV_BAR_EXT_HEIGHT: CGFloat = 200
+    
+    private let hasNotch = UIDevice.current.hasNotch
+    private var navHeight: CGFloat = 0
     
     private let navBar: UIView = {
         let view = UIView()
-        view.translatesAutoresizingMaskIntoConstraints = false
         view.backgroundColor = .init(red: CGFloat(0xDD / 255.0), green: CGFloat(0xDD / 255.0), blue: CGFloat(0xDD / 255.0), alpha: 1)
-        view.heightAnchor.constraint(equalToConstant: UIDevice.current.hasNotch ? NAV_BAR_HEIGHT * 2 : NAV_BAR_HEIGHT + STAT_BAR_HEIGHT).isActive = true
         return view
     }()
     
@@ -26,24 +28,36 @@ class ViewController: UIViewController {
         btn.translatesAutoresizingMaskIntoConstraints = false
         btn.setTitle("＋", for: .normal)
         btn.setTitleColor(.systemBlue, for: .normal)
-        btn.titleLabel?.font = UIFont.systemFont(ofSize: NAV_BAR_HEIGHT)
         btn.addTarget(self, action: #selector(plusItemTapped(_:)), for: .touchUpInside)
-        btn.constraintWidth(equalToConstant: NAV_BAR_HEIGHT, heightEqualToConstant: NAV_BAR_HEIGHT)
         return btn
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        navHeight = hasNotch ? NAV_BAR_HEIGHT * 2 : NAV_BAR_HEIGHT + STAT_BAR_HEIGHT
         view.addSubview(navBar)
-        navBar.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        navBar.frame = .init(x: 0, y: 0, width: view.frame.size.width, height: navHeight)
         navBar.addSubview(plusIconBarItem)
-        plusIconBarItem.bottomAnchor.constraint(equalTo: navBar.bottomAnchor).isActive = true
+        plusIconBarItem.titleLabel?.font = UIFont.systemFont(ofSize: NAV_BAR_HEIGHT)
+        plusIconBarItem.constraintWidth(equalToConstant: NAV_BAR_HEIGHT, heightEqualToConstant: NAV_BAR_HEIGHT)
+        plusIconBarItem.topAnchor.constraint(equalTo: navBar.topAnchor, constant: hasNotch ? NAV_BAR_HEIGHT : STAT_BAR_HEIGHT).isActive = true
         plusIconBarItem.trailingAnchor.constraint(equalTo: navBar.trailingAnchor, constant: -5).isActive = true
     }
     
+    let rotation45DTransform = CGAffineTransform(rotationAngle: .pi / 4)
     @objc func plusItemTapped(_ sender: UIButton) {
-        print("plus icon pressed")
+        if navBar.frame.size.height < NAV_BAR_EXT_HEIGHT {
+            UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.35, initialSpringVelocity: 5, options: .curveEaseInOut, animations: {
+                self.navBar.frame = CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.NAV_BAR_EXT_HEIGHT)
+                self.plusIconBarItem.transform = self.rotation45DTransform
+            })
+        } else {
+            UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.35, initialSpringVelocity: 5, options: .curveEaseInOut, animations: {
+                self.navBar.frame = CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.navHeight)
+                self.plusIconBarItem.transform = .identity
+            })
+        }
     }
     
 }
