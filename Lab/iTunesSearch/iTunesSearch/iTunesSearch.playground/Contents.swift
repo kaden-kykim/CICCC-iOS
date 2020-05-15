40ed6523734cc67a85c460a22a7178f4d6e53084
kaden-kykim <kaden.kykim@gmail.com>
2020-05-15 14:47:48 -0700

    [Lab 5.6] Add JSONDecoder and codable StoreItems and StoreItem (Lab 5.5)

--- Lab/iTunesSearch/iTunesSearch/iTunesSearch.playground/Contents.swift
+++ Lab/iTunesSearch/iTunesSearch/iTunesSearch.playground/Contents.swift
@@ -6,64 +6,70 @@ import PlaygroundSupport
 PlaygroundPage.current.needsIndefiniteExecution = true
 
 extension URL {
-    
     func withQueries(_ queries: [String: String]) -> URL? {
-        
         var components = URLComponents(url: self, resolvingAgainstBaseURL: true)
-        components?.queryItems = queries.flatMap { URLQueryItem(name: $0.0, value: $0.1) }
+        components?.queryItems = queries.map { URLQueryItem(name: $0.0, value: $0.1) }
         return components?.url
     }
 }
 
-struct StoreItem {
+struct StoreItems: Codable {
+    let results: [StoreItem]
     
-    var name: String
-    var artist: String
-    var description: String
-    var kind: String
-    var artworkURL: URL
+    enum CodingKeys: String, CodingKey {
+        case results
+    }
+}
+
+struct StoreItem: Codable {
+    let name: String
+    let artist: String
+    let kind: String
+    let artworkURL: URL
+    let description: String
     
-    init?(json: [String: Any]) {
+    enum CodingKeys: String, CodingKey {
+        case name = "trackName"
+        case artist = "artistName"
+        case kind
+        case artworkURL = "artworkUrl100"
+        case description
+    }
+    
+    enum AdditionalKeys: String, CodingKey {
+        case longDescription
+    }
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        name = try values.decode(String.self, forKey: CodingKeys.name)
+        artist = try values.decode(String.self, forKey: CodingKeys.artist)
+        kind = try values.decode(String.self, forKey: CodingKeys.kind)
+        artworkURL = try values.decode(URL.self, forKey: CodingKeys.artworkURL)
         
-        guard let name = json["trackName"] as? String,
-            let artist = json["artistName"] as? String,
-            let kind = json["kind"] as? String,
-            let artworkURLString = json["artworkUrl100"] as? String,
-            let artworkURL = URL(string: artworkURLString) else { return nil }
-        
-        self.name = name
-        self.artist = artist
-        self.kind = kind
-        self.artworkURL = artworkURL
-        
-        self.description = json["description"] as? String ?? json["longDescription"] as? String ?? ""
+        if let description = try? values.decode(String.self, forKey: CodingKeys.description) {
+            self.description = description
+        } else {
+            let additionalValues = try decoder.container(keyedBy: AdditionalKeys.self)
+            description = (try? additionalValues.decode(String.self, forKey: AdditionalKeys.longDescription)) ?? ""
+        }
     }
 }
 
 func fetchItems(matching query: [String: String], completion: @escaping ([StoreItem]?) -> Void) {
-    
     let baseURL = URL(string: "https://itunes.apple.com/search?")!
     
     guard let url = baseURL.withQueries(query) else {
-        
         completion(nil)
         print("Unable to build URL with supplied queries.")
         return
     }
     
     let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
-        
-        if let data = data,
-            let rawJSON = try? JSONSerialization.jsonObject(with: data),
-            let json = rawJSON as? [String: Any],
-            let resultsArray = json["results"] as? [[String: Any]] {
-            
-            let storeItems = resultsArray.flatMap { StoreItem(json: $0) }
-            completion(storeItems)
-            
+        if let data = data, let storeItems = try? JSONDecoder().decode(StoreItems.self, from: data) {
+            completion(storeItems.results)
         } else {
             print("Either no data was returned, or data was not serialized.")
-            
             completion(nil)
             return
         }
@@ -74,12 +80,13 @@ func fetchItems(matching query: [String: String], completion: @escaping ([StoreI
 
 
 let query: [String: String] = [
-    "term": "Inside Out 2015",
+    "term": "Frozen",
     "media": "movie",
     "lang": "en_us",
     "limit": "10"
 ]
 
 fetchItems(matching: query) { (items) in
-    print(items)
+    if let items = items { print(items) }
+    PlaygroundPage.current.finishExecution()
 }
