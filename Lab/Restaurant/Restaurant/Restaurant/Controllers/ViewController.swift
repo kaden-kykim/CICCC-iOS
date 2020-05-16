332608be8bc2dc24a644a46e2085777028739da4
kaden-kykim <kaden.kykim@gmail.com>
2020-05-16 16:44:26 -0700

    [Guided Project 5] Add Part 3-1: Project Setup

---
+++ Lab/Restaurant/Restaurant/Restaurant/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Restaurant
+//
+//  Created by Kaden Kim on 2020-05-16.
+//  Copyright © 2020 CICCC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
