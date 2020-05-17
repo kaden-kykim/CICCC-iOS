b9af07211be8f7bc8beaa24ebd4586354092f4f4
kaden-kykim <kaden.kykim@gmail.com>
2020-05-16 19:28:04 -0700

    [Guided Project 5] Add Part 7: Menu Details

--- Lab/Restaurant/Restaurant/Restaurant/Controllers/ViewController/MenuItemDetailViewController.swift
+++ Lab/Restaurant/Restaurant/Restaurant/Controllers/ViewController/MenuItemDetailViewController.swift
@@ -9,13 +9,33 @@
 import UIKit
 
 class MenuItemDetailViewController: UIViewController {
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var priceLabel: UILabel!
+    @IBOutlet var detailTextLabel: UILabel!
+    @IBOutlet var addToOrderButton: UIButton!
     
     var menuItem: MenuItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        addToOrderButton.layer.cornerRadius = 5.0
+        updateUI()
     }
     
+    private func updateUI() {
+        titleLabel.text = menuItem.name
+        priceLabel.text = String(format: "$ %.2f", menuItem.price)
+        detailTextLabel.text = menuItem.detailText
+    }
+    
+    @IBAction func orderButtonTapped(_ sender: UIButton) {
+        UIView.animate(withDuration: 0.3) {
+            self.addToOrderButton.transform = CGAffineTransform(scaleX: 1.5, y: 1.5)
+            self.addToOrderButton.transform = .identity
+        }
+    }
     
     /*
      // MARK: - Navigation
