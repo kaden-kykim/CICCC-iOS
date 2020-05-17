eac3966013d097673297376746f7f857ccd2d996
kaden-kykim <kaden.kykim@gmail.com>
2020-05-16 18:58:11 -0700

    [Guided Project 5] Add Part 6: Menu items

--- Lab/Restaurant/Restaurant/Restaurant/Controllers/ViewController/CategoryTableViewController.swift
+++ Lab/Restaurant/Restaurant/Restaurant/Controllers/ViewController/CategoryTableViewController.swift
@@ -9,14 +9,13 @@
 import UIKit
 
 class CategoryTableViewController: UITableViewController {
-
+    
     private let cellId = "CategoryCellIdentifier"
-    private let menuController = MenuController()
     private var categories = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        menuController.fetchCategories {
+        MenuController.shared.fetchCategories {
             if let categories = $0 {
                 self.categories = categories
                 self.updateUI(with: categories)
@@ -24,16 +23,16 @@ class CategoryTableViewController: UITableViewController {
         }
     }
     
-    func updateUI(with categories: [String]) {
+    private func updateUI(with categories: [String]) {
         DispatchQueue.main.async { self.tableView.reloadData() }
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return categories.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
         configure(cell, forItemAt: indexPath)
@@ -43,14 +42,14 @@ class CategoryTableViewController: UITableViewController {
     private func configure(_ cell: UITableViewCell, forItemAt indexPath: IndexPath) {
         cell.textLabel?.text = categories[indexPath.row].capitalized
     }
-
+    
     // MARK: - Navigation
-
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "MenuSegue" {
             let menuTVC = segue.destination as! MenuTableViewController
             menuTVC.category = categories[tableView.indexPathForSelectedRow!.row]
         }
     }
-
+    
 }
--- Lab/Restaurant/Restaurant/Restaurant/Controllers/ViewController/MenuTableViewController.swift
+++ Lab/Restaurant/Restaurant/Restaurant/Controllers/ViewController/MenuTableViewController.swift
@@ -11,82 +11,50 @@ import UIKit
 class MenuTableViewController: UITableViewController {
     
     var category: String!
-
+    
+    private let cellId = "MenuCellIdentifier"
+    private var menuItems = [MenuItem]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        title = category.capitalized
+        MenuController.shared.fetchMenuItems(forCategory: category) {
+            if let menuItems = $0 {
+                self.menuItems = menuItems
+                self.updateUI(with: menuItems)
+            }
+        }
     }
-
+    
+    private func updateUI(with menuItems: [MenuItem]) {
+        DispatchQueue.main.async { self.tableView.reloadData() }
+    }
+    
     // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return menuItems.count
     }
-
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        configure(cell, forItemAt: indexPath)
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    private func configure(_ cell: UITableViewCell, forItemAt indexPath: IndexPath) {
+        let menuItem = menuItems[indexPath.row]
+        cell.textLabel?.text = menuItem.name
+        cell.detailTextLabel?.text = String(format: "$%.2f", menuItem.price)
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
+    
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "MenuDetailSegue" {
+            let menuItemDetailVC = segue.destination as! MenuItemDetailViewController
+            menuItemDetailVC.menuItem = menuItems[tableView.indexPathForSelectedRow!.row]
+        }
     }
-    */
-
+    
 }
