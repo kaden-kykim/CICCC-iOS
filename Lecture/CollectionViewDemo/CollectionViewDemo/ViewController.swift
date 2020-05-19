2bacab6df7c116eabf5a76b6bedf5cd8d2b0c996
kaden-kykim <kaden.kykim@gmail.com>
2020-05-19 14:13:58 -0700

    [CollectionView (May 19)] Add Simple CollectionView programmatically

--- Lecture/CollectionViewDemo/CollectionViewDemo/ViewController.swift
+++ Lecture/CollectionViewDemo/CollectionViewDemo/ViewController.swift
@@ -9,12 +9,62 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var collectionView: UICollectionView!
+    
+    private let padding: CGFloat = 8
+    
+    override func loadView() {
+        super.loadView()
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.minimumInteritemSpacing = padding
+        layout.minimumLineSpacing = padding
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        view = collectionView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "Cell")
+        collectionView.dataSource = self
+        collectionView.delegate = self
     }
-
-
+    
 }
 
+// MARK: - CollectionView data source
+extension ViewController : UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 200
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
+        cell.backgroundColor = .green
+        return cell
+    }
+    
+}
+
+// MARK: - CollectionView delegate flow layout
+extension ViewController : UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let size = (collectionView.frame.width - 2 * padding) / 3.0
+        return CGSize(width: size, height: size)
+    }
+    
+}
+
+// MARK: - CollectionView delegate
+extension ViewController : UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print(indexPath)
+    }
+    
+}
