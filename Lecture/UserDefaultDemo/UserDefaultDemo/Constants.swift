90691d577d146f938acd484b216e2af3189a55b8
kaden-kykim <kaden.kykim@gmail.com>
2020-05-22 14:04:07 -0700

    [Persistence (May 22)] Add simple UserDefaults demo

---
+++ Lecture/UserDefaultDemo/UserDefaultDemo/Constants.swift
@@ -0,0 +1,21 @@
+//
+//  Constants.swift
+//  UserDefaultDemo
+//
+//  Created by Kaden Kim on 2020-05-22.
+//  Copyright © 2020 CICCC. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Constants {
+    
+    struct Dimension {
+        static let padding: CGFloat = 20
+    }
+    
+    struct UserDefalut {
+        static let bluetoothSwitch = "switch"
+    }
+}
--- Lecture/UserDefaultDemo/UserDefaultDemo/ViewController.swift
+++ Lecture/UserDefaultDemo/UserDefaultDemo/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var bluetoothSwitch: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let defaults = UserDefaults.standard
+        if defaults.object(forKey: Constants.UserDefalut.bluetoothSwitch) != nil {
+            bluetoothSwitch.isOn = defaults.bool(forKey: Constants.UserDefalut.bluetoothSwitch)
+        }
     }
 
-
+    @IBAction func saveSwitchState(_ sender: UISwitch) {
+        // save the state of switch
+        // -> "UserDefault"
+        
+        // 1. get the userDefaults object
+        let defaults = UserDefaults.standard
+        
+        // 2. save the state
+        defaults.set(sender.isOn, forKey: Constants.UserDefalut.bluetoothSwitch)
+    }
+    
 }
-
