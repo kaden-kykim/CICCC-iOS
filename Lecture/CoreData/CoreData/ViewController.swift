84650134b5c803244551d47f83b7fac199528195
kaden-kykim <kaden.kykim@gmail.com>
2020-06-12 21:17:34 -0700

    Create CoreData project

---
+++ Lecture/CoreData/CoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreData
+//
+//  Created by Kaden Kim on 2020-06-12.
+//  Copyright © 2020 Kaden Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
