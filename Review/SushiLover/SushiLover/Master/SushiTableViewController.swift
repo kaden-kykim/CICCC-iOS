7d4785786d6e2012058aad9cf278a4ca38a723e5
kaden-kykim <kaden.kykim@gmail.com>
2020-06-14 18:50:35 -0700

    [Re:SearchBar] Add SearchFooter

--- Review/SushiLover/SushiLover/Master/SushiTableViewController.swift
+++ Review/SushiLover/SushiLover/Master/SushiTableViewController.swift
@@ -8,11 +8,16 @@
 
 import UIKit
 
-class SushiTableViewController: UITableViewController {
+class SushiTableViewController: UIViewController {
+    
     private let cellId = "SushiCell"
     private var sushis: [Sushi] = Sushi.sushis()
     private var filteredSushis: [Sushi] = []
+    
+    private var tableView: UITableView!
     private let searchController = UISearchController(searchResultsController: nil)
+    private var searchFooter: SearchFooter!
+    private var searchFooterBottomConstraint: NSLayoutConstraint!
     
     private var isSearchbarEmpty: Bool {
         return searchController.searchBar.text?.isEmpty ?? true
@@ -24,22 +29,33 @@ class SushiTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
         /// Navigation title view
         let titleImageView = UIImageView(image: UIImage(named: "banner"))
         titleImageView.contentMode = .scaleAspectFill
         titleImageView.constraintWidth(equalToConstant: 150, heightEqualToConstant: 33)
         navigationItem.titleView = titleImageView
-        
+
         /// Table view
-        tableView.register(SubtitleCell.self, forCellReuseIdentifier: cellId)
+        setupTableView()
         
         /// Search Controller
         setupSearchController()
+        setupSearchFooter()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.navigationBar.prefersLargeTitles = false
+    }
+    
+    private func setupTableView() {
+        tableView = UITableView()
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(SubtitleCell.self, forCellReuseIdentifier: cellId)
+        view.addSubview(tableView)
+        tableView.anchors(topAnchor: view.safeAreaLayoutGuide.topAnchor, leadingAnchor: view.leadingAnchor, trailingAnchor: view.trailingAnchor, bottomAnchor: view.safeAreaLayoutGuide.bottomAnchor)
     }
     
     private func setupSearchController() {
@@ -50,20 +66,62 @@ class SushiTableViewController: UITableViewController {
         // ensure that the search bar doesn't remain on the screen if the user navigates to another view controller
         // while the UISearchController is active
         definesPresentationContext = true
+        
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(forName: UIResponder.keyboardDidShowNotification, object: nil, queue: .main) {
+            self.handleKeyboard(notification: $0)
+        }
+        notificationCenter.addObserver(forName: UIResponder.keyboardWillHideNotification, object: nil, queue: .main) {
+            self.handleKeyboard(notification: $0)
+        }
+    }
+    
+    private func setupSearchFooter() {
+        searchFooter = SearchFooter()
+        view.addSubview(searchFooter)
+        searchFooter.constraintHeight(equalToConstant: 44)
+        let constraints = searchFooter.anchors(topAnchor: nil, leadingAnchor: view.leadingAnchor, trailingAnchor: view.trailingAnchor, bottomAnchor: view.bottomAnchor)
+        searchFooterBottomConstraint = constraints.bottom
     }
     
     private func filterSushiFor(searchText: String, category: Sushi.Category? = nil) {
         filteredSushis = sushis.filter { $0.name.lowercased().contains(searchText.lowercased()) }
+        if isFiltering {
+            searchFooter.isFilteringToShow(filteredItemCount: filteredSushis.count, of: sushis.count)
+        } else {
+            searchFooter.isNotFiltering()
+        }
         tableView.reloadData()
     }
     
-    // MARK: - Table view data source
+    private func handleKeyboard(notification: Notification) {
+        guard notification.name == UIResponder.keyboardDidShowNotification else {
+            searchFooterBottomConstraint.constant = 0
+            view.layoutIfNeeded()
+            return
+        }
+        guard
+            let info = notification.userInfo,
+            let keyboardFrame = info[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue
+            else { return }
+        
+        let keyboardHeight = keyboardFrame.cgRectValue.size.height
+        searchFooterBottomConstraint.constant = -keyboardHeight
+        UIView.animate(withDuration: 0.1) {
+            self.view.layoutIfNeeded()
+        }
+    }
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+}
+
+// MARK: - Table view data source
+extension SushiTableViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return isFiltering ? filteredSushis.count : sushis.count
     }
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as! SubtitleCell
         let sushi = isFiltering ? filteredSushis[indexPath.row] : sushis[indexPath.row]
         cell.textLabel?.text = sushi.name
@@ -71,9 +129,11 @@ class SushiTableViewController: UITableViewController {
         return cell
     }
     
-    // MARK: - Table view delegate
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+}
+
+// MARK: - Table view delegate
+extension SushiTableViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let sushiDetailVC = SushiDetailViewController()
         sushiDetailVC.sushi = isFiltering ? filteredSushis[indexPath.row] : sushis[indexPath.row]
         navigationController?.pushViewController(sushiDetailVC, animated: true)
