213b95e92ae5648d259b71cfa7c256d7a1a308a0
kaden-kykim <kaden.kykim@gmail.com>
2020-06-14 22:10:18 -0700

    [Re:UserDefaults] Add switch for save/load setting value by UserDefaults

--- Review/UserDefaults/UserDefaults/ViewController.swift
+++ Review/UserDefaults/UserDefaults/ViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var bluetoothSwitch: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let defaults = UserDefaults.standard
+        if defaults.object(forKey: "switch") != nil {
+            bluetoothSwitch.isOn = defaults.bool(forKey: "switch")
+        }
     }
 
-
+    @IBAction func saveSwitchState(_ sender: UISwitch) {
+        // save the state of the switch
+        // -> "UserDefaults"
+        
+        // 1. get the userDefaults object
+        let defaults = UserDefaults.standard
+        
+        // 2. save the state
+        defaults.set(sender.isOn, forKey: "switch")
+    }
+    
 }
 
