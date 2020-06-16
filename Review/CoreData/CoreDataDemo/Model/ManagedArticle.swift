733eb30709a660d4694cc8b08670feb9ce16ae18
kaden-kykim <kaden.kykim@gmail.com>
2020-06-15 21:04:16 -0700

    [Re:CoreData] Rename project name: CoreData to CoreDataDemo   - Add Managed Data Model: Article, Source

---
+++ Review/CoreData/CoreDataDemo/Model/ManagedArticle.swift
@@ -0,0 +1,14 @@
+//
+//  ManagedArticle.swift
+//  CoreData
+//
+//  Created by Kaden Kim on 2020-06-15.
+//  Copyright © 2020 Kaden Kim. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class ManagedArticle: NSManagedObject {
+
+}
---
+++ Review/CoreData/CoreDataDemo/Model/ManagedSource.swift
@@ -0,0 +1,14 @@
+//
+//  ManagedSource.swift
+//  CoreData
+//
+//  Created by Kaden Kim on 2020-06-15.
+//  Copyright © 2020 Kaden Kim. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class ManagedSource: NSManagedObject {
+
+}
